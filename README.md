de53b7dbc717a133b66fb987273b02b08bd965d7
thaddziura <49488898+thaddziura@users.noreply.github.com>
2023-07-04 16:30:10 -0600

    Initial Website

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Sigma Chi Theta O Website
- Official website for the Sigma Chi Theta Omicron Chapter
+
+Official website for the Sigma Chi Theta Omicron Chapter
+
+Shoutout to Brother Max Molchanov '22 for the initial website
---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// Show full officer list if "more" button is clicked
+function showOfficers() {
+    var offBut = document.getElementById("officersButton");
+    var noShow = document.getElementsByClassName("not-shown");
+    var officer = document.getElementById("officers");
+  
+    if (offBut.innerHTML == "Less") { 
+      offBut.innerHTML = "More";
+      for (i = 0; i < noShow.length; i++)
+        noShow[i].style.display = "none";
+    } else {
+      offBut.innerHTML = "Less";
+      for (i = 0; i < noShow.length; i++)
+        noShow[i].style.display = "flex";
+    }
+    officer.scrollIntoView({behavior: "smooth"})
+}
